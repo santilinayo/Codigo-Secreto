96efccf3120511270bb5b7aeed1a488be7aa5601
santilinayo <santiago.linayo@gmail.com>
2022-01-27 20:23:19 -0300

    Final

--- app.js
+++ app.js
@@ -25,9 +25,7 @@ function encriptacion(e){
 
 if(!pattern.test(inputValue)){
     resultado.innerHTML ="Caracter INVALIDO"
-} if(inputValue===""){
-    resultado.innerHTML ="por favor escriba algo"
-} else{
+}  else{
     resultado.innerHTML=(inputValue.replaceAll("e","enter").replaceAll("i","imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat"));
 }
     
@@ -41,9 +39,8 @@ function desencriptacion(e){
     let inputValue = document.getElementById("texto").value;
     if(!pattern.test(inputValue)){
         resultado.innerHTML ="Caracter INVALIDO"
-    }if(inputValue===""){
-        resultado.innerHTML ="por favor escriba algo"
-    } else{
+    }
+     else{
         resultado.innerHTML=(inputValue.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u"))
     }
     
