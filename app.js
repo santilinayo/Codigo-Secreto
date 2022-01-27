b8f28dfdb9ff7d7d70d8637c396e4b915af81b50
santilinayo <santiago.linayo@gmail.com>
2022-01-27 20:02:03 -0300

    primera version

---
+++ app.js
@@ -0,0 +1,57 @@
+let encriptar = document.getElementById("encriptar");
+let desencriptar = document.getElementById("desencriptar");
+let resultado =document.getElementById("resultado");
+
+
+const pattern = new RegExp('^[a-z]');
+
+function copyToClipBoard() {
+
+    
+    
+    resultado.select();
+    document.execCommand('copy');
+
+    alert("Copied!");
+}
+
+
+
+function encriptacion(e){
+    e.preventDefault();
+    let inputValue = document.getElementById("texto").value;
+   
+
+
+if(!pattern.test(inputValue)){
+    resultado.innerHTML ="Caracter INVALIDO"
+} else{
+    resultado.innerHTML=(inputValue.replaceAll("e","enter").replaceAll("i","imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat"));
+}
+    
+
+
+
+}
+
+function desencriptacion(e){
+    e.preventDefault();
+    let inputValue = document.getElementById("texto").value;
+    
+    resultado.innerHTML=(inputValue.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u"))
+    
+}
+
+encriptar.onclick = encriptacion;
+desencriptar.onclick =desencriptacion;
+
+
+
+
+
+
+
+
+encriptar.onclick = encriptacion;
+desencriptar.onclick = desencriptacion;
+copy.onclick =copyToClipBoard;
