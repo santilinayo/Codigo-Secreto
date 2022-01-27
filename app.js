3c8a95d0cf89569a9758aada0c85f160a78b078a
santilinayo <santiago.linayo@gmail.com>
2022-01-27 20:16:21 -0300

    Se agregaron validaciones

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ function encriptacion(e){
 
 if(!pattern.test(inputValue)){
     resultado.innerHTML ="Caracter INVALIDO"
+} if(inputValue===""){
+    resultado.innerHTML ="por favor escriba algo"
 } else{
     resultado.innerHTML=(inputValue.replaceAll("e","enter").replaceAll("i","imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat"));
 }
@@ -37,8 +39,13 @@ if(!pattern.test(inputValue)){
 function desencriptacion(e){
     e.preventDefault();
     let inputValue = document.getElementById("texto").value;
-    
-    resultado.innerHTML=(inputValue.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u"))
+    if(!pattern.test(inputValue)){
+        resultado.innerHTML ="Caracter INVALIDO"
+    }if(inputValue===""){
+        resultado.innerHTML ="por favor escriba algo"
+    } else{
+        resultado.innerHTML=(inputValue.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u"))
+    }
     
 }
 
